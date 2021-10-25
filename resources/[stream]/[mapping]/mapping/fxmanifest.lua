aae3980448d93f2e1f679bfba3e90b0e6d773b75
xJunior <82475714+xJuniior@users.noreply.github.com>
2021-10-25 23:28:48 +0200

    mapping

---
+++ resources/[stream]/[mapping]/mapping/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'bodacious'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+client_script('client/main.lua')
+
+files({
+	'timecycle_mods_1.xml'
+})
+
+data_file('TIMECYCLEMOD_FILE')('timecycle_mods_1.xml')
+
+data_file 'TIMECYCLEMOD_FILE' 'gabz_mrpd_timecycle.xml'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+files {
+	'gabz_mrpd_timecycle.xml',
+	'interiorproxies.meta'
+}
+
+client_script {
+    "gabz_mrpd_entitysets.lua"
+}
+
+client_script '@protect/xDxDxDxDxD.lua'
